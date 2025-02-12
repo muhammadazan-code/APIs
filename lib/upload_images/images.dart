87e7ce81f396e8ef8fdb723bfffc1c0bcc6b7516
Code With Azan <azanjokhio54@gmail.com>
2025-02-12 13:52:36 +0500

    day 2 of learning Post APIs

---
+++ lib/upload_images/images.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class UploadImage extends StatefulWidget {
+  const UploadImage({super.key});
+
+  @override
+  State<UploadImage> createState() => _UploadImageState();
+}
+
+class _UploadImageState extends State<UploadImage> {
+  File? image;
+  final _picker = ImagePicker();
+  bool showSpinner = false;
+  Future getimage() async {
+    final pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+    if (PickedFile != null) {
+      image = File(pickedFile!.path);
+      setState(() {});
+    } else {
+      print('No Image');
+    }
+  }
+
+  Future<void> getloadImage() async {
+    setState(() {
+      showSpinner = true;
+    });
+    var stream = new http.ByteStream(image!.openRead());
+    stream.cast();
+    var length = await image!.length();
+    var uri = Uri.parse('https://fakestoreapi.com/products');
+    var request = new http.MultipartRequest('POST', uri);
+    request.fields['title'] = 'Static title';
+    var multipart = new http.MultipartFile('images', stream, length);
+    request.files.add(multipart);
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      setState(() {
+        showSpinner = false;
+      });
+      print('image uploaded');
+    } else {
+      setState(() {
+        showSpinner = false;
+      });
+      print('image not uploaded');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  /* When the user click the pick image button it will go to the storage of device. */
+                  getimage();
+                  setState(() {});
+                },
+                child: Container(
+                  child: image == null
+                      ? Center(
+                          child: Text(
+                            'Pick image',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 250,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Center(
+                              child: Image.file(
+                                File(image!.path).absolute,
+                                height: 150,
+                                width: 250,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      /*  When the user click the upload image m image will be uploaded */
+                      getloadImage();
+                      setState(() {});
+                    },
+                    child: Container(
+                      alignment: AlignmentDirectional.center,
+                      height: 80,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(15.0)),
+                      child: Text(
+                        'Upload Image',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
